5d0f280f18d4d7757c6b3513dd631699cb1aa30f
hamedmp <HamedMP@users.noreply.github.com>
2026-05-10 17:48:57 +0200

    fix(ci): cover e2e and docker startup

---
+++ shell/next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// This file is committed so non-root Docker dev containers do not need to
+// create it inside the bind-mounted shell directory during startup.
